d61673c3672bfafe14d351679ff020a6d3b98689
kazuyainoue0124 <88877589+kazuyainoue0124@users.noreply.github.com>
2024-06-18 11:50:09 +0000

    Add account activation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "bauer24tbp@gmail.com"
   layout "mailer"
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  host = 'zany-zebra-wjgw5gvx74r2gqr6-3000.app.github.dev'     # クラウドIDE
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
